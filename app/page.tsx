34d3162128fefc950a574ba4fd7ce44ab6860cb4
arai <niimaru09@gmail.com>
2024-10-14 21:50:08 +0900

    configure sandbox as the article

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,180 @@
-import Image from "next/image";
 
-export default function Home() {
+"use client"
+
+import { useEffect, useRef, useCallback, useState } from 'react';
+
+
+import { RealtimeClient } from "@openai/realtime-api-beta";
+import { ItemType } from '@openai/realtime-api-beta/dist/lib/client.js';
+
+import { WavRecorder, WavStreamPlayer } from '@/lib/wavtools/index';
+
+const LOCAL_RELAY_SERVER_URL: string =
+  process.env.REACT_APP_LOCAL_RELAY_SERVER_URL || '';
+
+
+export default function AudioTalk (){
+
+  const apiKey = process.env.OPENAI_API_KEY
+
+  const client = new RealtimeClient({ 
+      apiKey: apiKey,
+      dangerouslyAllowAPIKeyInBrowser: true 
+  })
+
+  
+
+  /**
+   * Instantiate:
+   * - WavRecorder (speech input)
+   * - WavStreamPlayer (speech output)
+   * - RealtimeClient (API client)
+   */
+    const wavRecorderRef = useRef<WavRecorder>(
+      new WavRecorder({ sampleRate: 24000 })
+    );
+    const wavStreamPlayerRef = useRef<WavStreamPlayer>(
+      new WavStreamPlayer({ sampleRate: 24000 })
+    );
+    const clientRef = useRef<RealtimeClient>(
+      new RealtimeClient(
+        LOCAL_RELAY_SERVER_URL
+          ? { url: LOCAL_RELAY_SERVER_URL }
+          : {
+              apiKey: apiKey,
+              dangerouslyAllowAPIKeyInBrowser: true,
+            }
+      )
+    );
+
+  const wavRecorder = new WavRecorder({ sampleRate: 24000 })
+  const wavStreamPlayer = new WavStreamPlayer({ sampleRate: 24000 })
+
+  const [isConnected, setIsConnected] = useState(false)
+  const [items, setItems] = useState<ItemType[]>([]);
+
+  /**
+   * Connect to conversation:
+   * WavRecorder taks speech input, WavStreamPlayer output, client is API client
+   */
+  const connectConversation = useCallback(async () => {
+    const client = clientRef.current;
+    const wavRecorder = wavRecorderRef.current;
+    const wavStreamPlayer = wavStreamPlayerRef.current;
+
+    // Set state variables
+    setIsConnected(true);
+    setItems(client.conversation.getItems());
+
+    // Connect to microphone
+    await wavRecorder.begin();
+
+    // Connect to audio output
+    await wavStreamPlayer.connect();
+
+    // Connect to realtime API
+    await client.connect();
+    client.sendUserMessageContent([
+      {
+        type: `input_text`,
+        text: `Hello!`,
+        // text: `For testing purposes, I want you to list ten car brands. Number each item, e.g. "one (or whatever number you are one): the item name".`
+      },
+    ]);
+
+    if (client.getTurnDetectionType() === 'server_vad') {
+      await wavRecorder.record((data) => client.appendInputAudio(data.mono));
+    }
+  }, []);
+
+  const disconnectConversation = useCallback(async () => {
+      setIsConnected(false);
+      setItems([]);
+  
+      client.disconnect();
+  
+      await wavRecorder.end();
+  
+      await wavStreamPlayer.interrupt();
+  }, []);
+
+  /**
+ * Core RealtimeClient and audio capture setup
+ * Set all of our instructions, tools, events and more
+ */
+  useEffect(() => {
+
+    // Set instructions
+    client.updateSession({ instructions: 'あなたは役にたつAIアシスタントです' });
+    // Set transcription, otherwise we don't get user transcriptions back
+    client.updateSession({ input_audio_transcription: { model: 'whisper-1' } });
+    // ユーザーが話終えたことをサーバー側で判断する'server_vad'に設定。（'manual'モードもある）
+    client.updateSession({turn_detection:{type:'server_vad'}})
+
+    // 不要
+    client.on('error', (event: any) => console.error(event));
+
+    console.log(client)
+
+    // 必要
+    client.on('conversation.interrupted', async () => {
+      console.log('interrupted')
+      const trackSampleOffset = await wavStreamPlayer.interrupt();
+      if (trackSampleOffset?.trackId) {
+        const { trackId, offset } = trackSampleOffset;
+        await client.cancelResponse(trackId, offset);
+      }
+    });
+
+    // 必要
+    client.on('conversation.updated', async ({ item, delta }: any) => {
+      console.log('convesation.updated')
+      const items = client.conversation.getItems();
+      console.log('items',items)
+      if (delta?.audio) {
+        wavStreamPlayer.add16BitPCM(delta.audio, item.id);
+      }
+      if (item.status === 'completed' && item.formatted.audio?.length) {
+        const wavFile = await WavRecorder.decode(
+          item.formatted.audio,
+          24000,
+          24000
+        );
+        item.formatted.file = wavFile;
+      }
+      setItems(items);
+    });
+
+    setItems(client.conversation.getItems());
+
+    return () => {
+      // cleanup; resets to defaults
+      client.reset();
+    };
+  }, []);
+
+  
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <>
+          <h1>最小限のrealtime APIを試す</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+          <div>
+          {items.map(item=>{
+              return (<>
+              <div>{item.role}：{JSON.stringify(item.formatted.transcript)}</div>
+              </>)
+          })}
+          </div>
+
+          <div>
+          {isConnected? 
+              <button onClick={disconnectConversation}>停止</button>:
+              <button onClick={connectConversation}>録音開始</button>
+          }
+
+          </div>
+          
+      </>
+  )
 }
